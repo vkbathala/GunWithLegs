707b1b2aa9bcdcb1b5dc11cfadb7c6cdd3acac80
ZealousElite120 <skar32@gatech.edu>
2021-02-14 15:44:43 -0500

    implemented basic RPG functionality

---
+++ Assets/Scenes/Soham Kar/SplashDamage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashDamage : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D hitCollider) // damage enemy if it is within range of explosion
+    {
+        Debug.Log(hitCollider.gameObject.name);
+    }
+}
